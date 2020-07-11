52b4433b2bc2bd416a52f5a5d3c5631740bb3e5a
syedrameezahmed18 <noir.ramsam@gmail.com>
2020-07-11 16:24:31 +0500

    CRUD initialization

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const hero = require("./importer.js")
 const express = require("express");
 const bodyParser = require("body-parser");
+const mysql = require("mysql")
 const app = express();
 
+var mysqlconnection = mysql.createConnection({
+    host:'localhost',
+    user: "root",
+    password: "rameez18",
+    database :"employeefulldb"
+});
+mysqlconnection.connect((err)=>{
+    if(!err)
+    console.log("no error");
+    else
+    console.log(`con failed ${JSON.stringify(err,undefined,2)}`);
+    /*this will give an error initially of authentication to fix this type 
+    ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY "rameez18";
+    flush privileges;   in the mysql workbench 
+    */
+})
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 var welcomed;
 
-app.use(express.static('./public'))
+//app.use(express.static('./public'))
 app.post("/profile",(req,res)=>{
     console.log(req.body);
     welcomed=req.body;
